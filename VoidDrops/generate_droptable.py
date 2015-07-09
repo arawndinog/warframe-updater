3fe3f276c93fccf399a199718cd9e0bb70dbdea3
Adrian <arawndinog@gmail.com>
2015-07-08 19:41:12 -0600

    various fix

--- VoidDrops/generate_droptable.py
+++ VoidDrops/generate_droptable.py
@@ -2,29 +2,27 @@ fname = 'VoidPCAsh.txt'
 output = open('lua_output.txt', 'w')
 
 with open(fname, 'r') as origin:
-	rotation = ""
 	for line in origin:
-		if 'TOWER' in line:
-			location = line.rstrip().split(" ")
-			tier = location[0] + " " + location[1]
-			mission = ' '.join(location[2:])
-			rotation = "" #every time it reaches tower/derelict, rotation refreshes
-			continue
-		elif 'DERELICT' in line:
-			location =  line.rstrip().split(" ")
-			tier = location[1]
-			mission =  ' '.join(location[2:])
-			rotation = ""
-			continue
-		elif 'Rotation' in line:
-			rotation = " " + line.rstrip(":\n").partition(" ")[2]
-			continue
-		elif not line.isspace():
-			reward = line.rstrip().split(" ")
-			i = 0
-			item = ""
-			while (i < len(reward)) and (reward[i] != "PRIME") and (reward[i] != "BLUEPRINT"):
-				item += reward[i]
-				i += 1
-			part = reward[len(reward)-1]
-		output.write('{"'+ tier + '","' + mission + rotation + '","' + item + '","' + part + '"},\n')
+		if not line.isspace():
+			if 'TOWER' in line:
+				location = line.rstrip().split(" ")
+				tier = "Tower " + location[1]
+				mission = ' '.join(location[2:]).title()
+				rotation = ""
+			elif 'DERELICT' in line:
+				location = line.rstrip().split(" ")
+				tier = "Derelict"
+				mission =  ' '.join(location[2:]).title()
+				rotation = ""
+			elif 'Rotation' in line:
+				rotation = " " + line.rstrip(":\n").partition(" ")[2]
+			else:
+				if 'PRIME' in line:
+					reward = line.rstrip().partition(" PRIME ")
+					item = reward[0]
+					part = reward[2]
+				else:
+					reward = line.rstrip().split(" ")
+					item = reward[0]
+					part = ' '.join(reward[1:])
+				output.write('	{"'+ tier + '","' + mission + rotation + '","' + item + '","' + part + '"},\n')
