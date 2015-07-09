ddc64c2101f97066001318b6b163984b1da878a7
Adrian <arawndinog@gmail.com>
2015-07-08 18:46:19 -0600

    update functionality

--- VoidDrops/generate_droptable.py
+++ VoidDrops/generate_droptable.py
@@ -2,11 +2,29 @@ fname = 'VoidPCAsh.txt'
 output = open('lua_output.txt', 'w')
 
 with open(fname, 'r') as origin:
-	tower_type = ""
+	rotation = ""
 	for line in origin:
 		if 'TOWER' in line:
-			tower_type = line
+			location = line.rstrip().split(" ")
+			tier = location[0] + " " + location[1]
+			mission = ' '.join(location[2:])
+			rotation = "" #every time it reaches tower/derelict, rotation refreshes
+			continue
+		elif 'DERELICT' in line:
+			location =  line.rstrip().split(" ")
+			tier = location[1]
+			mission =  ' '.join(location[2:])
+			rotation = ""
+			continue
+		elif 'Rotation' in line:
+			rotation = line.rstrip(":\n").partition(" ")[2]
 			continue
 		elif not line.isspace():
-			line.partition(" PRIME ")[0]
-		output.write(tower_type + line)
+			reward = line.rstrip().split(" ")
+			i = 0
+			item = ""
+			while (i < len(reward)) and (reward[i] != "PRIME") and (reward[i] != "BLUEPRINT"):
+				item += reward[i]
+				i += 1
+			part = reward[len(reward)-1]
+		output.write(tier + "," + mission + " " + rotation + "," + item + "," + part + "\n")
