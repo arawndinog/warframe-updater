ee45cb7734f6e4445ae28cdfcf23a0d13ba3f9cf
Adrian Wong <arawndinog@gmail.com>
2016-05-15 21:26:40 +0800

    minor

--- VoidDrops_auto/parse_prime.py
+++ VoidDrops_auto/parse_prime.py
@@ -1,15 +1,24 @@
+from html.parser import HTMLParser
 from urllib import request
-import io
 
 def main():
     response = request.urlopen('https://forums.warframe.com/forum/3-pc-update-build-notes/')
-    parsed_topics = io.StringIO(str(response.read()))
-    #link = parsed_updates.find('Prime Gear Drop Locations')
-    #print(link)
-    result_line = "stuff"
-    while 'Prime Gear Drop Locations' not in parsed_topics:
-        result_line = parsed_topics.readline()
-    print(result_line)
+    parser = MyHTMLParser()
+    parser.feed(str(response.read()))
+
+
+class MyHTMLParser(HTMLParser):
+    #def handle_starttag(self, tag, attrs):
+    #    if tag == "p":
+    #        print("attr:", attrs)
+            #print("Encountered a start tag:", tag)
+
+    #def handle_endtag(self, tag):
+    #    print("Encountered an end tag :", tag)
+
+    #def handle_data(self, data):
+    #    print("Encountered some data  :", data)
+
 
 if __name__ == '__main__':
     main()
