be1d59806a3662a5b555e547c5c7fa6955b8368f
Adrian Wong <arawndinog@gmail.com>
2016-05-26 21:25:51 +0800

    minor

---
+++ VoidDrops_auto/html_test.js
@@ -0,0 +1,11 @@
+var options = {
+  host: 'www.google.com',
+  port: 80,
+  path: '/index.html'
+};
+
+http.get(options, function(res) {
+  console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
